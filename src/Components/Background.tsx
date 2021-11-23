2b4581c138ce589bee3fbc101892cf2c14b3a26e
mr-piyson <mun.ateeq@gmail.com>
2021-11-23 20:19:44 +0300

    security issue

--- src/Components/Background.tsx
+++ src/Components/Background.tsx
@@ -3,15 +3,15 @@ import ClickEffect from "../Hooks/ClickEffect";
 import { useRef } from "react";
 
 const users = [
-  { name: "Ali", phone: "36860504", total: -8.0, date: "2021/12/12" },
-  { name: "john", phone: "35860504", total: 3.0, date: "2021/12/5" },
-  { name: "Jak", phone: "36890504", total: 90.0, date: "2021/9/5" },
-  { name: "Dewan", phone: "360504", total: -15.0, date: "2021/5/3" },
+  { name: "Ali", phone: "654651321", total: -8.0, date: "2021/12/12" },
+  { name: "john", phone: "654651321", total: 3.0, date: "2021/12/5" },
+  { name: "Jak", phone: "8464654654", total: 90.0, date: "2021/9/5" },
+  { name: "Dewan", phone: "54654654", total: -15.0, date: "2021/5/3" },
   { name: "Showman", phone: "66860504", total: 0.0, date: "2021/7/9" },
   { name: "Sara", phone: "76860504", total: 5.0, date: "2021/2/1" },
-  { name: "Ali", phone: "36860504", total: 8.0, date: "2021/12/12" },
+  { name: "Ali", phone: "464654651", total: 8.0, date: "2021/12/12" },
   { name: "Showman", phone: "66860504", total: 0.0, date: "2021/7/9" },
-  { name: "Jak", phone: "36890504", total: 90.0, date: "2021/9/5" },
+  { name: "Jak", phone: "654655465", total: 90.0, date: "2021/9/5" },
 ];
 export default function Background(props: any) {
   return (
